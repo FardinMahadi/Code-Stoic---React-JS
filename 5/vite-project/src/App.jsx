1d90f69b612b6e0a0cb303139123d08ea88b9347
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-10-11 10:35:41 +0600

    props

--- 5/vite-project/src/App.jsx
+++ 5/vite-project/src/App.jsx
@@ -1,5 +1,12 @@
+import Hello from "./components/Hello";
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Hello name="Fardin" message="Hi there!" />
+      <Hello name="Mahadi" message="Hello...." />
+    </div>
+  );
 }
 
 export default App;
--- 5/vite-project/src/components/Hello.jsx
+++ 5/vite-project/src/components/Hello.jsx
@@ -1,5 +1,18 @@
-function Hello() {
-  return <h1>Hello from a component!</h1>;
+// const name = "Fardin";
+
+// function displayMessage() {
+//   return "Wow!";
+// }
+
+function Hello(props) {
+  return (
+    <div>
+      <h1>
+        {props.message} {props.name}
+      </h1>
+      {/* <h1>Wow!</h1> */}
+    </div>
+  );
 }
 
 export default Hello;
